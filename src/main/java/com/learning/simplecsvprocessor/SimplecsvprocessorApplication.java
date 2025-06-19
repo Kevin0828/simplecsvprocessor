25285e2c737a38088e8b699831f3d306575a9917
Kevin Fontela <fontelakevin0828@gmail.com>
2025-06-19 00:14:46 -0400

    first commit

---
+++ src/main/java/com/learning/simplecsvprocessor/SimplecsvprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.learning.simplecsvprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecsvprocessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplecsvprocessorApplication.class, args);
+    }
+
+}
